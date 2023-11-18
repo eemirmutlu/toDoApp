9550f2b7223eca8c7d3bd9f41786c96833a33b14
Emir Mutlu <emirmutlu169@icloud.com>
2023-11-18 11:04:16 +0300

    html kodlarını düzenledim.

---
+++ script.js
@@ -0,0 +1,7 @@
+/* <li class="d-flex justify-content-between align-items-center bg-info px-3 rounded-2">
+    <p class="mt-3">İnput value</p>
+    <div class="d-flex gap-3">
+        <i class="fa-solid fa-thumbs-up fa-beat"></i>
+        <i class="fa-solid fa-trash fa-bounce"></i>
+    </div>
+</li> */
